f60a97066a885549c54ccb9fdab411ceac82c4e8
Abdi <abdullahia0215@gmail.com>
2023-10-08 17:53:55 -0500

    added in routes to pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,32 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, NavLink} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import DetailsPage from '../DetailsPage/Details'
+import AddMovieForm from '../MovieForm/MovieForm'
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
       <Router>        
+        <nav>
+          <ul className='navBar'>
+            <li><NavLink to="/">Movie list</NavLink></li>
+            <li><NavLink to="/addmovie">Add movie form</NavLink></li>
+          </ul>
+        </nav>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
-        {/* Details page */}
-
-        {/* Add Movie page */}
+        <Route path="/detail/:id">
+          <DetailsPage />
+        </Route>
+        <Route path="/addmovie">
+          <AddMovieForm />
+        </Route>
       </Router>
     </div>
   );
 }
-
 
 export default App;
